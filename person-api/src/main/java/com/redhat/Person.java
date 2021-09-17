6333e4cabbf6da183abcbdc94db48b3b08cfd541
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2021-09-17 16:40:40 -0300

    adding a sample api

---
+++ person-api/src/main/java/com/redhat/Person.java
@@ -0,0 +1,10 @@
+package microservices.person.src.main.java.com.redhat;
+
+public class Person {
+    public String id;
+    public String name;
+    public Integer age;
+    public String city;
+    public String state;
+    public String phone;
+}
